cb09f1c7a795455d5c481428f858c7a39599b41b
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2019-02-17 23:23:21 +0100

    Blackboard base start

---
+++ Unity_Project/Assets/GOAP System/Elements/Blackboard/Variable_GS.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variable_GS {
+
+    //Content fields
+    [SerializeField] private string name;
+    [SerializeField] private string id;
+    [SerializeField] private object value;
+    [SerializeField] private bool protect;
+
+    //Follow this TODO plz
+
+}
