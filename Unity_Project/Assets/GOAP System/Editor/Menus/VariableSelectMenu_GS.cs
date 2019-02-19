584b595cd3ad9db72bb5beff02c78fa080d04492
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2019-02-19 21:57:03 +0100

    Variable selection assembly access update

--- Unity_Project/Assets/GOAP System/Editor/Menus/VariableSelectMenu_GS.cs
+++ Unity_Project/Assets/GOAP System/Editor/Menus/VariableSelectMenu_GS.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.Reflection;
 
 namespace GOAP_S.UI
 {
@@ -12,7 +13,7 @@ namespace GOAP_S.UI
         static private string _variable_name;
         //static private System.Enum _variable_type = GOAP_S.PT.NodeUIMode;
         //Content fields 
-        private ArrayList listed_elements;
+        private ArrayList listed_elements = new ArrayList();
 
         Vector2 scrollPos = new Vector2(0, 0);
         string t = "This is a string inside a Scroll view!";
@@ -40,33 +41,63 @@ namespace GOAP_S.UI
 
             //Variable name field
             _variable_name = EditorGUILayout.TextField("Name",_variable_name,GUILayout.Width(120), GUILayout.ExpandWidth(true));
-            //Variable type field
-            // _variable_type = EditorGUILayout.EnumFlagsField("Type",_variable_type);
-            //Variable value field
+
+            //Test scroll
+            GUILayout.BeginVertical();
+            scrollPos = GUILayout.BeginScrollView(scrollPos, GUILayout.ExpandWidth(true),GUILayout.ExpandHeight(true));
+            foreach(string str in listed_elements)
+            {
+                GUILayout.Label(str, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
+            }
+
+            //Test
+            if (listed_elements.Count > 0)
+            {
+                string[] stockArr = new string[listed_elements.Count - 1];
+                stockArr = (string[])listed_elements.ToArray(typeof(string));
+                EditorGUILayout.Popup(0, stockArr);
+            }
+
+            GUILayout.EndScrollView();
+            GUILayout.EndVertical();
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Get asm classes", GUILayout.Width(100), GUILayout.Height(10), GUILayout.ExpandHeight(true)))
+            {
+                Assembly executing_asm = Assembly.GetAssembly(typeof(Component));
+                foreach(System.Type type in executing_asm.GetTypes())
+                {
+                    if (type.IsClass && type.IsPublic)
+                    {
+                        listed_elements.Add(type.ToString());
+                    }
+                }
+                Debug.Log(listed_elements.Count);
+            }
+            
+
+            if (GUILayout.Button("Clear",GUILayout.ExpandHeight(true)))
+            {
+                t = "";
+            }
+            GUILayout.EndHorizontal();
+
+            
+           GUILayout.FlexibleSpace(); 
 
             GUILayout.BeginHorizontal();
             //Add button
-            if(GUILayout.Button("Add",_target_node_editor.UI_configuration.node_modify_button_style,GUILayout.ExpandWidth(true)))
+            if (GUILayout.Button("Add", _target_node_editor.UI_configuration.node_modify_button_style, GUILayout.ExpandWidth(true),GUILayout.ExpandHeight(true)))
             {
                 //Add the new variable if the data is correct
 
             }
             //Close button
-            if (GUILayout.Button("Close", _target_node_editor.UI_configuration.node_modify_button_style, GUILayout.ExpandWidth(true)))
+            if (GUILayout.Button("Close", _target_node_editor.UI_configuration.node_modify_button_style, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true)))
             {
                 editorWindow.Close();
             }
             GUILayout.EndHorizontal();
-
-            EditorGUILayout.BeginHorizontal();
-            scrollPos = EditorGUILayout.BeginScrollView(scrollPos, GUILayout.Width(100), GUILayout.Height(100));
-            GUILayout.Label(t);
-            EditorGUILayout.EndScrollView();
-            if (GUILayout.Button("Add More Text", GUILayout.Width(100), GUILayout.Height(100)))
-                t += " \nAnd this is more text!";
-            EditorGUILayout.EndHorizontal();
-            if (GUILayout.Button("Clear"))
-                t = "";
         }
 
         public override void OnClose()
@@ -75,6 +106,11 @@ namespace GOAP_S.UI
         }
 
         //Functionality Methods =======
+        /*public override Vector2 GetWindowSize()
+        {
+
+        }*/
+
         public Variable_GS GenerateVariable()
         {
             return null;
