0ec9f9c5d5adb088a95aeaf1fc7e1176c3d2c29c
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2019-02-25 16:42:54 +0100

    Field select menu base added

---
+++ Unity_Project/Assets/GOAP System/Editor/Menus/FieldSelectMenu_GS.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+namespace GOAP_S.UI
+{
+    public class FieldSelectMenu_GS : ScriptableWizard
+    {
+        //Content fields
+        private int _selected_field_index = -1;
+        private string _selected_field_path = "";
+
+        //Funciotnality Methods =======
+        public void CreateDropdown()
+        {
+            DisplayWizard<FieldSelectMenu_GS>("Field Select Menu");
+        }
+
+
+
+    }
+}
